9c69eed93b57888c8581017a2d8149b20a539d43
fyeung21 <yeung.fiona@hotmail.com>
2019-04-28 18:40:08 -0700

    96% of css done

--- script.js
+++ script.js
@@ -13,41 +13,30 @@
 
 document.addEventListener("DOMContentLoaded", function() {
     
-    const navLinks = document.querySelectorAll(navigation);
+    const elem = document.querySelector('.main-carousel');
+    const flkty = new Flickity(elem, {
+        // options
+        cellAlign: 'center',
+        contain: true,
+        draggable: true,
+        groupCells: 4,
+        autoPlay:2000
+    });
 
-    const navigation = [
-        'about',
-        'shop',
-        'featured',
-        'updates'
-    ];
-
-navLinks.forEach(function(navigation)) {
+    const navLinks = document.querySelectorAll('#navigation li a');
     
-
-  // add click listener for the element in this iteration of the loop
-  // how can you get the "href" attribute value of the clicked element to create a string that corresponds the id of the correct section?
-  // get the section by its id using the string from the last step
-  // scroll to that section with .scrollIntoView()
-});
- }
-
-
-document.getElementById('about').scrollIntoView(behavior:'smooth')
-
-
-var elem = document.querySelector('.main-carousel');
-var flkty = new Flickity( elem, {
-  // options
-  cellAlign: 'center',
-  contain: true;
-  draggable: true;
-  groupCells: 4;
-  autoPlay:true;
-});
-
-// element argument can be a selector string
-//   for an individual element
-var flkty = new Flickity( '.main-carousel', {
-  // options
+    navLinks.forEach(function(link) {
+        link.addEventListener("click", function(event){
+            console.log(event);
+            document.querySelector('#about').scrollIntoView({behavior: 'smooth'})
+            
+        });
+        // document.getElementById('about').scrollIntoView({behavior:'smooth'})
+        
+        // add click listener for the element in this iteration of the loop
+        // how can you get the "href" attribute value of the clicked element to create a string that corresponds the id of the correct section?
+        // get the section by its id using the string from the last step
+        // scroll to that section with .scrollIntoView()
+    });
+    
 });
