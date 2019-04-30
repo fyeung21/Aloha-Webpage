7e35d0d8232e2470ca3fcbb1faad78fd81815926
fyeung21 <yeung.fiona@hotmail.com>
2019-04-30 03:36:41 -0700

    Responsive done except header, JS done except for if statment on button

--- script.js
+++ script.js
@@ -1,42 +1,35 @@
-// console.log ('Hello World');
-
-// let myName = 'MyWebsite';
-
-// myName = 'My new name';
-
-// alert(myName);
-
-// //
-
-// let nameEL = document.getElementByID('name');
-
 
 document.addEventListener("DOMContentLoaded", function() {
     
     const elem = document.querySelector('.main-carousel');
     const flkty = new Flickity(elem, {
-        // options
-        cellAlign: 'center',
+
+        cellAlign: 'left',
         contain: true,
         draggable: true,
-        groupCells: 4,
-        autoPlay:2000
+        prevNextButtons: false,
+        autoPlay:5000
     });
 
     const navLinks = document.querySelectorAll('#navigation li a');
     
     navLinks.forEach(function(link) {
         link.addEventListener("click", function(event){
-            console.log(event);
-            document.querySelector('#about').scrollIntoView({behavior: 'smooth'})
+            event.preventDefault();
+            document.querySelector(event.target.hash).scrollIntoView({behavior: 'smooth'})
             
         });
-        // document.getElementById('about').scrollIntoView({behavior:'smooth'})
-        
-        // add click listener for the element in this iteration of the loop
-        // how can you get the "href" attribute value of the clicked element to create a string that corresponds the id of the correct section?
-        // get the section by its id using the string from the last step
-        // scroll to that section with .scrollIntoView()
+
     });
     
+    
+    let submitClick = document.getElementById('submit-button');
+
+    submitClick.addEventListener("click", function(event){
+
+    event ='Thanks For Subscribing!';
+
+    alert(event);
+    });
+
 });
